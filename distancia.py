6a082a6a84265c58a20714fe34f38eacbdc97e8e
Noxurge <renan.medeiros9@gmail.com>
2023-11-24 13:11:45 -0300

    Distância - 1016

--- distancia.py
+++ distancia.py
@@ -1,4 +1,3 @@
-x1, y1 = map(float, input().split())
-x2, y2 = map(float, input().split())
-distancia = (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5
-print("{:.4f}".format(distancia))
+Y = int(input())
+Y = Y * 2
+print("{} minutos".format(Y))
