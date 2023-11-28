27e2a15e506a0b6b2cf09c123392307572bb6280
Noxurge <renan.medeiros9@gmail.com>
2023-11-28 20:43:15 -0300

    Animal

---
+++ 1049.py
@@ -0,0 +1,19 @@
+primeiro = input()
+segundo = input()
+terceiro = input()
+
+especie = {
+    "vertebrado": {
+        "ave": {"carnivoro": "aguia", "onivoro": "pomba"},
+        "mamifero": {"onivoro": "homem", "herbivoro": "vaca"},
+    },
+    "invertebrado": {
+        "inseto": {"hematofago": "pulga", "herbivoro": "lagarta"},
+        "anelideo": {"hematofago": "sanguessuga", "onivoro": "minhoca"},
+    },
+}
+
+animal = especie.get(primeiro, {}).get(segundo, {}).get(terceiro)
+
+if animal:
+    print(animal)
