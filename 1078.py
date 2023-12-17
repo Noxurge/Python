a5745a111ecf0410d9a3f9498b6ef56e9aba59cf
Noxurge <renan.medeiros9@gmail.com>
2023-12-16 23:27:27 -0300

    Tabuada

--- 1078.py
+++ 1078.py
@@ -1,6 +1,13 @@
 N = int(input())
+<<<<<<< HEAD
 
 if 2 < N < 1000:
     for i in range(1, 11):
         mult = N * i
         print("{} x {} = {}".format(i, N, mult))
+=======
+if 2 < N < 1000:
+    for i in range(1, 11, 1):
+        A = i * N
+        print("{} x {} = {}".format(i, N, A))
+>>>>>>> 5af2d57d02cb071bef60c1f7ce92248faa15dc5e
